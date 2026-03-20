27808011d64935b574bcb20022be545c06d8d5ad
siyanda sokhela <SiyandaSokhela@gmail.com>
2026-03-20 17:29:23 +0200

    new recipe api

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import { ENV } from "./src/config/env.js";
+
+export default {
+  schema: "./src/db/schema.js",
+  out: "./src/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: ENV.DATABASE_URL,
+  },
+};
